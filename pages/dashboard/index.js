3c4ab66a483b196c0e66ea5a46e2656e1162a2d9
Gabriel Rozeno <gabrozenodev@gmail.com>
2023-09-25 17:35:02 -0300

    feat dashboard navbar w/ dynamic data finished

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,32 @@
+const onLoadUserInfo = () => {
+  // add email element
+  const email = localStorage.getItem("@WalletApp:userEmail");
+  const name = localStorage.getItem("@WalletApp:userName");
+
+  const navbarUserInfo = document.getElementById("navbar-user-container");
+  const navbarUserAvatar = document.getElementById("user-avatar");
+
+  const emailElement = document.createElement("p");
+  const emailText = document.createTextNode(email);
+  emailElement.appendChild(emailText);
+
+  navbarUserInfo.appendChild(emailElement);
+
+  // logout element
+  const logoutElement = document.createElement("a");
+  const logoutText = document.createTextNode("Sair");
+  logoutElement.appendChild(logoutText);
+
+  navbarUserInfo.appendChild(logoutElement);
+
+  // avatar user initial
+  const navbarUserAvatarElement = document.createElement("h3");
+  const navbarUserAvatarText = document.createTextNode(name.charAt(0));
+  navbarUserAvatarElement.appendChild(navbarUserAvatarText);
+
+  navbarUserAvatar.appendChild(navbarUserAvatarElement);
+};
+
 window.onload = () => {
-  localStorage.getItem('@WalletApp:userEmail')
-}
+  onLoadUserInfo();
+};
